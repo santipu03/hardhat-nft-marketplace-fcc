a72bb52be046a895cd4dc1401612fc8d10880566
santipu <santipujolsau@gmail.com>
2022-11-02 16:21:31 +0100

    created deploy script for the NFT marketplace

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -167,9 +167,3 @@ contract NftMarketplace {
         return s_proceeds[seller];
     }
 }
-
-// 1. `listItem`: List NFTs on the marketplace
-// 2. `buyItem`: Buy the NFTs
-// 3. `cancelItem`: Cancel a listing
-// 4. `updateListing`: Update price
-// 5. `withdrawProceeds`: Withdrae payment for my bought NFTs
