ee96e853a9c5b8026740b616efea3873f47ffff2
santipu <santipujolsau@gmail.com>
2022-11-03 18:39:23 +0100

    added chainId for localhost network

--- hardhat.config.js
+++ hardhat.config.js
@@ -23,6 +23,9 @@ module.exports = {
             chainId: 31337,
             blockConfirmations: 1,
         },
+        localhost: {
+            chainId: 31337,
+        },
         goerli: {
             url: GOERLI_RPC_URL,
             accounts: [PRIVATE_KEY],
