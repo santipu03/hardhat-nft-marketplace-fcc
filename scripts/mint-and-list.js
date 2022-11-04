454d07c52e714c18eb78ed9966d7772f6ab2f269
santipu <santipujolsau@gmail.com>
2022-11-04 17:36:19 +0100

    created script in utils folder to move blocks of localhost chain, used in mint-and-list script

--- scripts/mint-and-list.js
+++ scripts/mint-and-list.js
@@ -1,4 +1,5 @@
-const { ethers } = require("hardhat")
+const { ethers, network } = require("hardhat")
+const { moveBlocks } = require("../utils/move-blocks")
 
 const price = ethers.utils.parseEther("0.1")
 
@@ -20,6 +21,10 @@ async function mintAndList() {
     await tx.wait(1)
 
     console.log("Listed!")
+
+    if (network.config.chainId == 31337) {
+        await moveBlocks(2, (sleepAmount = 1000))
+    }
 }
 
 mintAndList()
