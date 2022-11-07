df271746627ff8b28212e9118d0d3310f07ae542
santipu <santipujolsau@gmail.com>
2022-11-07 17:37:27 +0100

    created script to buy an item on the marketplace

---
+++ scripts/buy-item.js
@@ -0,0 +1,28 @@
+const { ethers, network } = require("hardhat")
+const { moveBlocks } = require("../utils/move-blocks")
+
+const TOKEN_ID = 0
+
+async function buyItem() {
+    const nftMarketplace = await ethers.getContract("NftMarketplace")
+    const basicNft = await ethers.getContract("BasicNft")
+    const listing = await nftMarketplace.getListing(basicNft.address, TOKEN_ID)
+    const price = listing.price.toString()
+
+    console.log("Buying item...")
+    const tx = await nftMarketplace.buyItem(basicNft.address, TOKEN_ID, { value: price })
+    await tx.wait(1)
+
+    console.log("NFT Bought")
+
+    if (network.config.chainId == "31337") {
+        await moveBlocks(2, (sleepAmount = 1000))
+    }
+}
+
+buyItem()
+    .then(() => process.exit(0))
+    .catch((e) => {
+        console.error(e)
+        process.exit(1)
+    })
